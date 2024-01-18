a441f5e521d0281bffdb08b1e0bcce0d2768a6df
eduardo <eduardolanza244@gmail.com>
2024-01-18 20:36:17 -0300

    codigo consertado

--- desafio.js
+++ desafio.js
@@ -7,6 +7,6 @@ if (xp < 1000) {
     console.log(" O Herói " + nomeDoPersonagem + " está no nível de " + "Bronze")
 } else if (xp > 5000 && xp <= 8000) {
     console.log(" O Herói " + nomeDoPersonagem + " está no nível de " + "Platina Diamante")
-} else if (xp > 8000 && xp <= 9000){
+} else if (xp > 8000 && xp <= 9000) {
     console.log("O Herói" + nomeDoPersonagem + " está no nível " + "Ascendente")
 }
